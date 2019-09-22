e90b621c3178241c2d29a3ecdc4fafd91b3c368e
Aline Sanches <sanches.aline6@gmail.com>
2019-09-22 11:37:44 -0300

    Reusing instatiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -60,17 +60,9 @@ public class SellerDaoJDBC implements SellerDao
 			rs = ps.executeQuery();
 			if(rs.next())
 			{
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
+				Department dep = instatiateDepartment(rs);
 				
-				Seller seller = new Seller();
-				seller.setId(rs.getInt("Id"));
-				seller.setName(rs.getString("Name"));
-				seller.setEmail(rs.getString("Email"));
-				seller.setBaseSalary(rs.getDouble("BaseSalary"));
-				seller.setBirthDate(rs.getDate("BirthDate"));
-				seller.setDeparment(dep);
+				Seller seller = instatiateSeller(rs,dep);
 				
 				return seller;
 			}
@@ -87,6 +79,28 @@ public class SellerDaoJDBC implements SellerDao
 		}
 		
 		
+	}
+
+	private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException
+	{
+		Seller seller = new Seller();
+		seller.setId(rs.getInt("Id"));
+		seller.setName(rs.getString("Name"));
+		seller.setEmail(rs.getString("Email"));
+		seller.setBaseSalary(rs.getDouble("BaseSalary"));
+		seller.setBirthDate(rs.getDate("BirthDate"));
+		seller.setDeparment(dep);
+		
+		return seller;
+	}
+
+	private Department instatiateDepartment(ResultSet rs) throws SQLException
+	{
+		Department dep = new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		
+		return dep;
 	}
 
 	@Override
